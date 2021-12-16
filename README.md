904c305084e9ddb13f164d66d7c38076f62c27ef
tugsem <busebs08@gmail.com>
2021-12-16 21:55:34 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# rockpaperscissors
+ Classic rps game to play on your console.
---
+++ script.js
@@ -0,0 +1,67 @@
+
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const options = document.getElementsByClassName("options");
+const score = document.getElementById("score");
+const moves= document.getElementById("moves");
+
+rock.value= "rock";
+paper.value= "paper";
+scissors.value= "scissors";
+
+
+//computer selection
+function computerPlay(){
+    const options = ["rock", "paper", "scissors"];
+        let choice =Math.floor(Math.random()*3);   
+        return options[choice];
+}
+
+ rock.addEventListener("click", playRound);
+ paper.addEventListener("click", playRound);
+ scissors.addEventListener("click", playRound);
+
+//single round. return string declared who has won.
+function playRound(e){
+    let playerSelection = e.target.value;
+    let computerSelection = computerPlay();
+    let result = "";
+        if(playerSelection == computerSelection) {
+            result = "It's a drawn";
+            moves.innerText = `You choose ${playerSelection}, computer choose ${computerSelection}. ${result}. `;
+            return result;
+        } 
+        switch(playerSelection) {
+            case "rock": 
+            result =  (computerSelection == "paper") ? "You lose" :  "You win";
+            break;
+            case "paper": 
+            result = (computerSelection == "rock") ?  "You win" :  "You lose";
+            break;
+            case "scissors":
+            result = (computerSelection == "paper") ?  "You win" :  "You lose" ;
+            break;
+        }
+    
+        moves.innerText = `You choose ${playerSelection}, computer choose ${computerSelection}. ${result}. `;
+        game(result);
+        return result;
+}
+
+// keeps score and return score table of one round.
+const scoreTable = (result) => {
+    let userScore = 0;
+    let compScore = 0;
+    return (result) => {
+        if (result == "You win") {
+             ++userScore;
+        } else if (result == "You lose") {
+             ++compScore;
+        }
+         score.innerText = `${userScore} - ${compScore}`;
+    }
+}
+const game = scoreTable();
+
+ 
